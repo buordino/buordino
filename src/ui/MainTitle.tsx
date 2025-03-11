11f0db58a9a6a400952e65299fee495cba7dbdf4
unknown <mh.naderi1386@gmail.com>
2025-03-11 23:56:50 +0330

    finish title of main

--- src/ui/MainTitle.tsx
+++ src/ui/MainTitle.tsx
@@ -3,11 +3,11 @@ import DownloadButton from "./DownloadButton";
 const MainTitle = () => {
   return (
     <section>
-      <div className="bg-stone-100 text-center space-y-[2rem] py-[16rem] px-4">
+      <div className="bg-stone-100 dark:bg-black text-center space-y-[2rem] py-[16rem] px-4">
         <h1 className="text-[#FF6347] font-bold text-2xl sm:text-4xl ">
           با بردینو برنده هر چیزی شو
         </h1>
-        <p className="text-sm sm:text-xl">
+        <p className="text-sm sm:text-xl dark:text-white">
           به هزاران کاربر بپیوند و با کوییز و قرعه‌کشی جوایز روزانه ببر.
         </p>
         <DownloadButton text="دانلود اپلیکیشن" size="small" href="#" />
