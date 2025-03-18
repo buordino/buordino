14bedb778c75ee992023c3d8826d509cb70eb925
unknown <mh.naderi1386@gmail.com>
2025-03-18 16:18:02 +0330

    add a background img to our main

--- src/ui/MainTitle.tsx
+++ src/ui/MainTitle.tsx
@@ -4,10 +4,10 @@ const MainTitle = () => {
   return (
     <section>
       <div className="bg-stone-100 dark:bg-black text-center space-y-[2rem] py-[16rem] px-4 bg-[url(/images/day-bg.jpg)] bg-no-repeat bg-center bg-cover">
-        <h1 className="text-[#FF6347] font-bold text-2xl sm:text-4xl ">
+        <h1 className="text-[#FF6347] font-bold text-3xl sm:text-4xl ">
           با بردینو برنده هر چیزی شو
         </h1>
-        <p className="text-sm sm:text-xl dark:text-white">
+        <p className="text-sm sm:text-xl text-white">
           به هزاران کاربر بپیوند و با کوییز و قرعه‌کشی جوایز روزانه ببر.
         </p>
         <DownloadButton text="دانلود اپلیکیشن" size="small" href="#" />
