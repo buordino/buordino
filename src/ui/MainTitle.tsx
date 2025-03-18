b0c9f694e0e3cdb9dba2384f01e28a1b8afd2fb3
unknown <mh.naderi1386@gmail.com>
2025-03-18 16:11:51 +0330

    add a background img

--- src/ui/MainTitle.tsx
+++ src/ui/MainTitle.tsx
@@ -3,7 +3,7 @@ import DownloadButton from "./DownloadButton";
 const MainTitle = () => {
   return (
     <section>
-      <div className="bg-stone-100 dark:bg-black text-center space-y-[2rem] py-[16rem] px-4">
+      <div className="bg-stone-100 dark:bg-black text-center space-y-[2rem] py-[16rem] px-4 bg-[url(/images/day-bg.jpg)] bg-no-repeat bg-center bg-cover">
         <h1 className="text-[#FF6347] font-bold text-2xl sm:text-4xl ">
           با بردینو برنده هر چیزی شو
         </h1>
